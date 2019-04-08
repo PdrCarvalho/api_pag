0f50a9f4987730eea52c83336bb94540b2c6454b
carvalhao <carvalho.pedro.cardoso@gmail.com>
2019-04-07 23:17:21 -0300

    create routes with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import Header from "./component/Header/index"
-import Main from "./pages/main/index"
 import "./styles.css"
-import api from "./service/api"
+import Routes from "./routes";
 class App extends Component {
   render() {
     return (
       <div className="App">
       <Header/>
-      <Main/>
+      <Routes/>
       </div>
     );
   }
