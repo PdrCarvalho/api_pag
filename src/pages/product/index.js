1e777c4f476335272a430f476230eef2ae64699f
carvalhao <carvalho.pedro.cardoso@gmail.com>
2019-04-07 23:51:01 -0300

    finish basic project

--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -1,8 +1,31 @@
 import React,{Component} from 'react';
+import api from '../../service/api'
+import "./styles.css"
+import {Link} from "react-router-dom";
 export default class Product extends Component{
+    state = {
+        product: {},
+    }
+    async componentDidMount(){
+        const {id}= this.props.match.params;
+        const response = await api.get(`/products/${id}`);
+        this.setState({product:response.data});
+    }
     render(){
+        const {product}= this.state;
         return (
-            <h1>Description</h1>
+            <div>
+            <div className ="productInfo">
+            <h1>{product.title}</h1>
+            <p>{product.description}</p>
+            <p>
+                URL: <a href={product.url}>{product.url}</a>
+            </p>
+            </div>
+            <div className="actions2">
+                <Link to="/">Back</Link>
+            </div>
+            </div>
         );
     }
 } 
